dc5837f52820eb762586b5162787b6a669358015
kabilan-7 <murugankabilan39@gmail.com>
2024-12-20 08:13:00 +0530

    change

--- SwapTwoNumbers.java
+++ SwapTwoNumbers.java
@@ -5,9 +5,11 @@ public class SwapTwoNumbers {
         int a=2;
         int b=4;
         a=a^b;
+        System.out.println("a=-->"+a);
         b=a^b;
+        System.out.println("b-->"+b);
         a=a^b;
-        System.out.println(a +" "+ b);
+        System.out.println(a);
 
     }
 }
